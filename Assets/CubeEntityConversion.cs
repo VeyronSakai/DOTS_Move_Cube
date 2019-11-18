fd1235d4d2c1b3e7b236807e849372bb642b6f3e
Yuki Sakai <yuukisakai123@gmail.com>
2019-11-18 22:55:33 +0900

    Create CubeEntityConversion

---
+++ Assets/CubeEntityConversion.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[RequiresEntityConversion]
+public class CubeEntityConversion : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        
+    }
+}
