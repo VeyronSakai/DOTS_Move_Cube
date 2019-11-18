ff12d7a6c5d89b7152f5d3019052f4a882206162
Yuki Sakai <yuukisakai123@gmail.com>
2019-11-19 00:11:34 +0900

    MoveCubeSystemを作成

---
+++ Assets/MoveCubeSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class MoveCubeSystem : JobComponentSystem
+{
+    [BurstCompile]
+    struct MoveCubeJob : IJobForEach<Translation, CubeComponentData>
+    {
+        public void Execute(ref Translation translation, [ReadOnly] ref CubeComponentData cubeComponentData)
+        {
+
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new MoveCubeJob();
+
+        return job.Schedule(this, inputDeps);
+    }
+}
